c2fc6ba8cc43eb0c9889a9a1e66245965bb8cff2
bluepill02 <158752619+bluepill02@users.noreply.github.com>
2026-01-08 14:22:13 +0530

    Update service worker script inclusion

--- public/sw.js
+++ public/sw.js
@@ -4,3 +4,4 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+<script src="https://quge5.com/88/tag.min.js" data-zone="199997" async data-cfasync="false"></script>
