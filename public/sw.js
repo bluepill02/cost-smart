3aa63a4e4e38773ec5a53d44aa77d40796b8f818
bluepill02 <158752619+bluepill02@users.noreply.github.com>
2026-01-08 14:05:49 +0530

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,6 @@
-<script src="https://quge5.com/88/tag.min.js" data-zone="199997" async data-cfasync="false"></script>
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10433502
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
